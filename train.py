d6ce742ab572889831fd3e8e024dfd37047cb049
pengyuanzhuo <pengyuanzhuo@163.com>
2019-07-02 20:59:21 +0800

    fix

--- train.py
+++ train.py
@@ -1,12 +1,11 @@
 import torch
-import data.voc as voc
-import data.collate as collate
-
+from data import Resize, VOCDetection, detection_collate
 
 if __name__ == '__main__':
-    transform = aug.Resize(300)
+    transform = Resize(300)
     vocdataset = VOCDetection('/workspace/dataset/VOCdevkit', transform=transform)
-    vocloader = data.DataLoader(vocdataset, batch_size=4, shuffle=True, collate_fn=collate.detection_collate)
+    vocloader = torch.utils.data.DataLoader(vocdataset, batch_size=4, shuffle=True,
+                                            collate_fn=detection_collate)
     for img, target in vocloader:
         print(img.shape)
         print(target)
