c21a8cc796e3bfd65944b08383882733fc538986
pengyuanzhuo <pengyuanzhuo@163.com>
2019-07-04 23:13:12 +0800

    Compose

--- data/augmentations.py
+++ data/augmentations.py
@@ -1,5 +1,26 @@
 import cv2
 
+
+class Compose(object):
+    """Composes several augmentations together.
+    Args:
+        transforms (List[Transform]): list of transforms to compose.
+    Example:
+        >>> augmentations.Compose([
+        >>>     transforms.CenterCrop(10),
+        >>>     transforms.ToTensor(),
+        >>> ])
+    """
+
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img, boxes=None, labels=None):
+        for t in self.transforms:
+            img, boxes, labels = t(img, boxes, labels)
+        return img, boxes, labels
+
+
 class Resize(object):
     def __init__(self, size=300):
         self.size = size
