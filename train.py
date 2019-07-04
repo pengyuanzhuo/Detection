e217279f0067ac6ac15a6a4beb00986b397e974a
pengyuanzhuo <pengyuanzhuo@qiniu.com>
2019-07-04 14:55:54 +0800

    for test

--- train.py
+++ train.py
@@ -1,12 +1,11 @@
 import torch
 from data import Resize, VOCDetection, detection_collate
+from models.ssd import build_ssd
+from config import Config as cfg
 
 if __name__ == '__main__':
-    transform = Resize(300)
-    vocdataset = VOCDetection('/workspace/dataset/VOCdevkit', transform=transform)
-    vocloader = torch.utils.data.DataLoader(vocdataset, batch_size=4, shuffle=True,
-                                            collate_fn=detection_collate)
-    for img, target in vocloader:
-        print(img.shape)
-        print(target)
-        break
+    model = build_ssd(cfg)
+    inputs = torch.zeros((2, 3, 300, 300))
+    print(inputs.shape)
+    default_box, conf, loc = model(inputs)
+    print(default_box.shape)
