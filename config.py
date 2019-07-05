2fbfed2970db5da74e1721b75d6b1807bad258ca
pengyuanzhuo <pengyuanzhuo@qiniu.com>
2019-07-05 17:45:31 +0800

    cfg

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    data_root = '/Users/pengyuanzhuo/data/VOCdevkit'
+    data_root = '/Users/qiniu/data/VOCdevkit'
     train_set = [('2007', 'trainval')]
     val_set = [('2007', 'test')]
 
@@ -9,10 +9,12 @@ class Config:
     ratios = [[2], [3, 2], [3, 2], [3, 2], [2], [2]]
     min_scale = 0.2
     max_scale = 0.9
+    variances = [1.0, 1.0] # target transform trick in ssd
+    threshold = 0.5 # default box match iou threshold
 
     epochs = 1
     workers = 4
-    batch_size = 32
+    batch_size = 1
     lr = 0.001
     weight_decay = 5e-4
     momentum = 0.9
